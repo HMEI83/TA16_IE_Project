748015b429863339cb1718707153ba89d7f64ee9
HMEI83 <101045819+HMEI83@users.noreply.github.com>
2022-08-27 21:23:00 +1000

    change word cloud layout
    change word cloud layout

--- ie-app/src/components/pages/learnPg.js
+++ ie-app/src/components/pages/learnPg.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import WordCloud from '../wordCloud/Wordcloud';
 import InputHandler from '../wordCloud/InputHandler';
+
 
 export default function Learning() {
   return (
     <>
       <h1 className='learning'>LEARNING</h1>
       <InputHandler />
-      {/* <WordCloud /> */}
     </>
   );
 }
--- ie-app/src/components/wordCloud/InputHandler.js
+++ ie-app/src/components/wordCloud/InputHandler.js
@@ -1,27 +1,32 @@
 import React, { useState } from "react";
-import { TagCloud } from 'react-tagcloud'
+import { TagCloud } from "react-tagcloud";
 import useTrait from "./useTrait";
+import "./wordCloud.css";
+import Button from '@mui/material/Button';
 
 function wordFreq(string) {
-  if (string === null) return
-  var words = string.replace(/[.]/g, '').split(/\s/);
+  if (string === null) return;
+  var words = string.replace(/[.]/g, "").split(/\s/);
   var freqMap = {};
-  words.forEach(function(w) {
-      if (!freqMap[w]) {
-          freqMap[w] = 0;
-      }
-      freqMap[w] += 1;
+  words.forEach(function (w) {
+    if (!freqMap[w]) {
+      freqMap[w] = 0;
+    }
+    freqMap[w] += 1;
   });
-  const result = Object.entries(freqMap).map(([value, count]) => ({value, count}))
-  console.log(result)
+  const result = Object.entries(freqMap).map(([value, count]) => ({
+    value,
+    count,
+  }));
+  console.log(result);
   return result;
 }
 
 const InputHandler = () => {
   const [enteredText, setEnteredText] = useState("");
-  const str = useTrait("")
-  const [submittedText, setSubmittedText] = useState(null);
-  const [dt, setDt] = useState([])
+  const str = useTrait("");
+  //const [submittedText, setSubmittedText] = useState(null);
+  const [dt, setDt] = useState([]);
 
   const textChangeHandler = (i) => {
     setEnteredText(i.target.value);
@@ -29,41 +34,47 @@ const InputHandler = () => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    str.set(enteredText)
+    str.set(enteredText);
     setEnteredText("");
-    setDt(wordFreq(str.get()))
+    setDt(wordFreq(str.get()));
   };
 
   return (
-    <div className="App">
+    <div className="word-cloud">
       <h1>Get user input</h1>
-      {/* <form onSubmit={submitHandler}> */}
-      <input
+      <textarea
+        className="text-area"
+        rows="5"
+        cols="50"
         placeholder="type something"
         type="text"
         value={enteredText}
         onChange={textChangeHandler}
       />
-      <button type="submit" onClick={submitHandler}>
+
+      
+      {/* <Button variant="contained" type="submit" onClick={submitHandler}>Submit</Button>; */}
+      <button type="submit" onClick={submitHandler} className="btn">
         Submit
       </button>
-      {/* </form> */}
       <TagCloud
+        minSize={12}
+        maxSize={35}
         tags={dt}
         className="simple-cloud"
         onClick={(tag) => {
-          const value = prompt('Edit tag value', tag.value)
+          const value = prompt("Edit tag value", tag.value);
           if (value == null) {
-            return
+            return;
           }
-          const newTag = { value, count: tag.count }
+          const newTag = { value, count: tag.count };
           const newData = dt.map((t) => {
             if (t.value === tag.value) {
-              return newTag
+              return newTag;
             }
-            return t
-          })
-          setDt(newData)
+            return t;
+          });
+          setDt(newData);
         }}
       />
     </div>
