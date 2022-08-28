aee20a587720fd61c7b320900a8027c4d3ffe97e
HMEI83 <101045819+HMEI83@users.noreply.github.com>
2022-08-28 11:03:15 +1000

    Finish final page
    Finish final page

---
+++ ie-app/src/components/slangQuiz/FinalPage.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Card from "./Card";
+
+import "./FinalPage.css";
+
+const FinalPage = ({
+    score,
+    setScore,
+    setShowFinalPage,
+    setShowStartingPage,
+    topScore,
+    setTopScore,
+}) => {
+    const clickHanlder = () => {
+        if (score > topScore){
+            setTopScore(score);
+        }
+
+        setShowFinalPage(false);
+        setShowStartingPage(true);
+        setScore(0);
+    };
+
+    return (
+        <Card>
+            <h1 className="heading">You reached the end of the game!</h1>
+            <h3 className="primary_text">Your final score is: </h3>
+            <h3 className="final_score">{score}</h3>
+
+            <button className="play_again_btn">Play Again</button>
+        </Card>
+    );
+};
+
+export default FinalPage
