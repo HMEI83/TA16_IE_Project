f59c55dc9cdb4baaa7cbbf1dfe4a9b0c18edaa59
HMEI83 <101045819+HMEI83@users.noreply.github.com>
2022-09-01 19:49:15 +1000

    New grammar check intro
    New grammar check intro

--- ie-app/src/components/pages/WriteCheckPg.js
+++ ie-app/src/components/pages/WriteCheckPg.js
@@ -3,16 +3,18 @@ import WritingHandler from "../writingAssessment/WritingHandler";
 import "./WriteCheck.css";
 
 const WriteCheck = () => {
-    return (
-        <div>
-            <div className="headContent">Welcome to our word cloud and have fun.
-                We want to make our website more interesting and encourage users to interact with us. Whether you want to learn English or keep up with Australian news, enter some words so that you can see the word frequency. For example, if you copy an article into this box, you can see the keywords, it is the word that appears most frequently in the article
-            </div>
-            <div className="writingTitleDIv">Writing Check</div>
-            <div className="inputDiv">{<WritingHandler/>}</div>
-            <div></div>
-        </div>
-    );
-}
+  return (
+    <div>
+      <div className="headContent">
+        For the new learners, we have a special offer. You can practice spelling
+        characters in this box, we provide grammar check features. In this way,
+        you cannot get bored with grammar as you learn it. Come and try it on!
+      </div>
+      <div className="writingTitleDIv">Writing Check</div>
+      <div className="inputDiv">{<WritingHandler />}</div>
+      <div></div>
+    </div>
+  );
+};
 
 export default WriteCheck;
