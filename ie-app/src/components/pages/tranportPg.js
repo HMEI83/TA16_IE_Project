71e2d984f860b84abfa67f59b22ae329e743e1da
Jack <61994603+Fantastisity@users.noreply.github.com>
2022-09-17 23:44:43 +1000

    Update tranportPg.js

--- ie-app/src/components/pages/tranportPg.js
+++ ie-app/src/components/pages/tranportPg.js
@@ -3,6 +3,45 @@ import "./transportationPg.css";
 import Accordion from "./Accordion";
 
 const Transport = () => {
+  var map = {};
+  map["full fare"] = 1;
+  map["concession"] = 2;
+  map["weekday"] = 4;
+  map["weekend"] = 5;
+  map["public holiday"] = 5;
+  map["daily fare"] = 6;
+  map["2 hour fare"] = 7;
+  map["zone1"] = 8;
+  map["zone2"] = 9;
+  map["zone12"] = 10;
+
+  map[(1 << 1) | (1 << 4) | (1 << 6) | (1 << 8)] = 9.2;
+  map[(1 << 1) | (1 << 4) | (1 << 6) | (1 << 9)] = 6.2;
+  map[(1 << 1) | (1 << 4) | (1 << 6) | (1 << 10)] = 9.2;
+  map[(1 << 1) | (1 << 4) | (1 << 7) | (1 << 8)] = 4.6;
+  map[(1 << 1) | (1 << 4) | (1 << 7) | (1 << 9)] = 3.1;
+  map[(1 << 1) | (1 << 4) | (1 << 7) | (1 << 10)] = 4.6;
+  map[(1 << 1) | (1 << 5) | (1 << 6) | (1 << 8)] = 6.7;
+  map[(1 << 1) | (1 << 5) | (1 << 6) | (1 << 9)] = 6.2;
+  map[(1 << 1) | (1 << 5) | (1 << 6) | (1 << 10)] = 6.7;
+  map[(1 << 1) | (1 << 5) | (1 << 7) | (1 << 8)] = 4.6;
+  map[(1 << 1) | (1 << 5) | (1 << 7) | (1 << 9)] = 3.1;
+  map[(1 << 1) | (1 << 5) | (1 << 7) | (1 << 10)] = 4.6;
+
+  map[(1 << 2) | (1 << 4) | (1 << 6) | (1 << 8)] = 4.6;
+  map[(1 << 2) | (1 << 4) | (1 << 6) | (1 << 9)] = 3.1;
+  map[(1 << 2) | (1 << 4) | (1 << 6) | (1 << 10)] = 4.6;
+  map[(1 << 2) | (1 << 4) | (1 << 7) | (1 << 8)] = 2.3;
+  map[(1 << 2) | (1 << 4) | (1 << 7) | (1 << 9)] = 1.55;
+  map[(1 << 2) | (1 << 4) | (1 << 7) | (1 << 10)] = 2.3;
+  map[(1 << 2) | (1 << 5) | (1 << 6) | (1 << 8)] = 3.35;
+  map[(1 << 2) | (1 << 5) | (1 << 6) | (1 << 9)] = 3.1;
+  map[(1 << 2) | (1 << 5) | (1 << 6) | (1 << 10)] = 3.35;
+  map[(1 << 2) | (1 << 5) | (1 << 7) | (1 << 8)] = 2.3;
+  map[(1 << 2) | (1 << 5) | (1 << 7) | (1 << 9)] = 1.55;
+  map[(1 << 2) | (1 << 5) | (1 << 7) | (1 << 10)] = 2.3;
+
+
   const [option1, setOption1] = useState("full fare");
   const [option2, setOption2] = useState("weekday");
   const [option3, setOption3] = useState("daily fare");
@@ -25,9 +64,8 @@ const Transport = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setResult(result);
-    // alert(`The name you entered was: ${name}`);
-    alert(`The name you entered was: ${option1}`);
+    setResult(map[(1 << map[option1]) | (1 << map[option2]) | (1 << map[option3]) | (1 << map[option4])]);
+    console.log(map[option1], map[option2], map[option3], map[option4]);
   };
 
   const options1 = [
@@ -74,11 +112,11 @@ const Transport = () => {
     },
     {
       label: "Zone2",
-      value: "Zone2",
+      value: "zone2",
     },
     {
       label: "Zone 1 & 2",
-      value: "Zone 1 & 2",
+      value: "zone12",
     },
   ];
 
@@ -117,7 +155,7 @@ const Transport = () => {
           </div>
           <div>
             <h2>4. Select a Zone</h2>
-            <select  value={option4} onChange={handleChangeOption4}>
+            <select value={option4} onChange={handleChangeOption4}>
               {options4.map((option) => (
                 <option value={option.value}>{option.label}</option>
               ))}
@@ -125,6 +163,9 @@ const Transport = () => {
           </div>
           <button type="submit">Calculate fare</button>
         </form>
+        <div>
+          {result}
+        </div>
       </div>
     </div>
   );
