a4c15586f0147b35b45fb8e61ea1e5f992e6525c
HMEI83 <101045819+HMEI83@users.noreply.github.com>
2022-09-17 22:56:29 +1000

    Change myki calculator
    Change myki calculator

--- ie-app/src/components/pages/tranportPg.js
+++ ie-app/src/components/pages/tranportPg.js
@@ -3,86 +3,128 @@ import "./transportationPg.css";
 import Accordion from "./Accordion";
 
 const Transport = () => {
-  const [peakValue, setpeakValue] = useState(0);
-  const [offPeakValue, setOffPeakValue] = useState(0);
-  const [weekendsValue, setWeakendsValue] = useState(0);
+  const [option1, setOption1] = useState("full fare");
+  const [option2, setOption2] = useState("weekday");
+  const [option3, setOption3] = useState("daily fare");
+  const [option4, setOption4] = useState("zone1");
+  const [result, setResult] = useState(9.20);
 
-  const handleChangePeak = (event) => {
-    setpeakValue(event.target.value);
+  const handleChangeOption1 = (event) => {
+    setOption1(event.target.value);
   };
 
-  const handleChangeOffPeak = (event) => {
-    setOffPeakValue(event.target.value);
+  const handleChangeOption2 = (event) => {
+    setOption2(event.target.value);
   };
-  const handleChangeWeekends = (event) => {
-    setWeakendsValue(event.target.value);
+  const handleChangeOption3 = (event) => {
+    setOption3(event.target.value);
+  };
+  const handleChangeOption4 = (event) => {
+    setOption4(event.target.value);
   };
 
-  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setResult(result);
+    // alert(`The name you entered was: ${name}`);
+    alert(`The name you entered was: ${option1}`);
+  };
+
+  const options1 = [
+    {
+      label: "Full fare",
+      value: "full fare",
+    },
+    {
+      label: "Concession",
+      value: "concession",
+    },
+  ];
+
+  const options2 = [
+    {
+      label: "Weekday",
+      value: "weekday",
+    },
+    {
+      label: "Weekend",
+      value: "weekend",
+    },
+    {
+      label: "Public Holiday",
+      value: "public holiday",
+    },
+  ];
+
+  const options3 = [
+    {
+      label: "Daily fare",
+      value: "daily fare",
+    },
+    {
+      label: "2 hour fare",
+      value: "2 hour fare",
+    },
+  ];
+
+  const options4 = [
+    {
+      label: "Zone1",
+      value: "zone1",
+    },
+    {
+      label: "Zone2",
+      value: "Zone2",
+    },
+    {
+      label: "Zone 1 & 2",
+      value: "Zone 1 & 2",
+    },
+  ];
 
   return (
-    <div style={{backgroundColor: "#F0BA98", height: "1780px"}}>
+    <div style={{ backgroundColor: "#F0BA98", height: "1780px" }}>
       <div className="headerBg">Public Transport in Victoria</div>
-          <div className="container">
-      <Accordion />
-    </div>
+      <div className="container">
+        <Accordion />
+      </div>
       <br></br>
       <div className="main-myki">
-        <p>
-          Here's an attempted quick calculator to work out your weekly Myki
-          Money fare, based on the number of days per week you travel at
-          different times:
-        </p>
-        <br />
-        <p>
-          <label>Peak weekdays:</label>
-          <input
-            className="input-label"
-            type="number"
-            min="0"
-            max="5"
-            onChange={handleChangePeak}
-            value={peakValue}
-          />
-          @ $9.00
-        </p>
-        <br />
-        <p>
-          <label>off-peak weekdays:</label>
-          <input
-            className="input-label"
-            type="number"
-            min="0"
-            max="5"
-            onChange={handleChangeOffPeak}
-            value={offPeakValue}
-          />
-          @ $6.30
-        </p>
-        <br />
-        <p>
-          <label>Weekends:</label>
-          <input
-            className="input-label"
-            type="number"
-            min="0"
-            max="2"
-            onChange={handleChangeWeekends}
-            value={weekendsValue}
-          />
-          @ $6.50
-        </p>
-        <br />
-
-        <p>
-          <label>Myki Money cost per week:</label>
-          <strong>
-            <span>
-              {" "}
-              ${peakValue * 9.0 + offPeakValue * 6.3 + weekendsValue * 6.5}
-            </span>
-          </strong>
-        </p>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <h2>1. Select a fare type</h2>
+            <select value={option1} onChange={handleChangeOption1}>
+              {options1.map((option) => (
+                <option value={option.value}>{option.label}</option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <h2>2. Select a day</h2>
+            <select value={option2} onChange={handleChangeOption2}>
+              {options2.map((option) => (
+                <option value={option.value}>{option.label}</option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <h2>3. Select a product</h2>
+            <select  value={option3} onChange={handleChangeOption3}>
+              {options3.map((option) => (
+                <option value={option.value}>{option.label}</option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <h2>4. Select a Zone</h2>
+            <select  value={option4} onChange={handleChangeOption4}>
+              {options4.map((option) => (
+                <option value={option.value}>{option.label}</option>
+              ))}
+            </select>
+          </div>
+          <button type="submit">Calculate fare</button>
+        </form>
       </div>
     </div>
   );
