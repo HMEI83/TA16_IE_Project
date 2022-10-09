fc1e03ba2baae9c15c772fb48b02aa6b87612817
Siyu(Jack) You <61994603+Fantastisity@users.noreply.github.com>
2022-10-09 11:34:37 +1100

    Update App.js

--- ie-app/src/App.js
+++ ie-app/src/App.js
@@ -6,14 +6,10 @@ import Home from './components/pages/homePg';
 import Life from './components/pages/lifePg';
 import Entertainment from './components/pages/entertainPg';
 import ExplorePg from './components/landmark/explorePg';
-import LandmarkMap from './components/landmark/LandmarkMap';
-import ArtworkMap from './components/landmark/ArtworkMap';
-import Quizzes from './components/pages/quizzesPg';
+import VICISHMap from './components/landmark/VICISHMap';
 import SlangQuiz from './components/pages/slangQuizPg';
 import EnglishQuiz from './components/pages/englishQuiz';
 import Bank from './components/pages/bankPg';
-// import Footer from "./components/general/Footer";
-import VocabPg from './components/pages/VocabPg';
 import WriteCheck from './components/pages/WriteCheckPg';
 import Diet from './components/pages/dietPg';
 import { RecipeReq } from "./components/recipe/recipeReqHandler";
@@ -21,9 +17,10 @@ import { MenuPlanner } from './components/menuplan/menuplanHandler';
 import PTV from './components/pages/ptvPg';
 import ScrollToTop from "./components/general/scrollToTop.js"
 import Transport from './components/pages/tranportPg';
-import MusicMap from './components/landmark/MusicMap';
 import StatisticsPg from './components/pages/statisticsPg';
-
+import Artwork from './components/landmark/artworkPg';
+import Landmark from './components/landmark/landmarkPg';
+import MusicVenue from './components/landmark/musicPg';
 function App() {
   return (
     <div className='App'>
@@ -35,25 +32,22 @@ function App() {
         <Route path='/life' element={<Life/>} />
         <Route path='/entertainments' element={<Entertainment/>} />
         <Route path='/explore-melb' element={<ExplorePg/>} />
-        <Route path='/landmark-map' element={<LandmarkMap/>} />
-        <Route path='/artwork-map' element={<ArtworkMap/>} />
-        <Route path='/music-map' element={<MusicMap/>} />
+        <Route path='/map' element={<VICISHMap/>} />
         <Route path='/slang' element={<SlangQuiz/>} />
         <Route path='/english-quiz' element={<EnglishQuiz/>} />
-        <Route path='/catch-keyword' element={<VocabPg />}/>
         <Route path='/writing' element={<WriteCheck />}/>
         <Route path='/diet' element={<Diet/>} />
         <Route path='/recipe' element={<RecipeReq/>} />
         <Route path='/ptv' element={<PTV/>} />
         <Route path='/bank' element={<Bank/>} />
         <Route path='/meal-plan' element={<MenuPlanner/>} />
-        <Route path='/transport' element={<Transport/>} />\
-        <Route path='/statistics' element={<StatisticsPg/>} />\
+        <Route path='/statistics' element={<StatisticsPg/>} />
+        <Route path='/transport' element={<Transport/>} />
+        <Route path='/landmarks' element={<Landmark/>}/>
+        <Route path='/artworks' element={<Artwork/>}/>
+        <Route path='/music-venue' element={<MusicVenue/>}/>
       </Routes>
     </Router>
-
-    {/* <div><Footer/></div> */}
-    
     </div>
   );
 }
