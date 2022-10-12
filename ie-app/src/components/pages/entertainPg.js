4ce87371e4b8202c2bb76525053251ce75833aa7
Siyu(Jack) You <61994603+Fantastisity@users.noreply.github.com>
2022-10-13 10:01:32 +1100

    Delete ie-app/src/components directory

--- ie-app/src/components/pages/entertainPg.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Entertainment() {
-  return (
-    <>
-      <h1 className='entertainment'>Entertainment</h1>
-    </>
-  );
-}
--- ie-app/src/components/pages/ptvPg.js
+++
@@ -1,7 +0,0 @@
-export default function PTV() {
-    return(
-        <h1 style={{marginTop: "10%"}}>
-            PTV
-        </h1>
-    )
-}
